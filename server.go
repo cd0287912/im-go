ed6bbbcbfe24b102e0717d89e5a86980b1b09860
忘不了oh <cd0287912@126.com>
2026-02-27 22:32:14 +0800

    v0.2

--- server.go
+++ server.go
@@ -3,23 +3,57 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP   string
 	Port int
+
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	Message   chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return &server
 }
 
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("已连接")
+	fmt.Println("用户上线了")
+	this.mapLock.Lock()
+
+	user := NewUser(conn)
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "上线了")
+	// sendMsg := "[" + user.Addr + "]" + user.Name + ":" + "上线了。。。。。"
+	// this.Message <- sendMsg
+
+	select {}
 }
 
 func (this *Server) Start() {
@@ -29,6 +63,7 @@ func (this *Server) Start() {
 		return
 	}
 	defer listener.Close()
+	go this.ListenMessager()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
